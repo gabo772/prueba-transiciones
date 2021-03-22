ece108ff6777b74d96e43d02c96fbdaa148c5c51
Gabriel Ramirez <gabriel.ramirez.lemus@gmail.com>
2021-03-22 04:49:25 -0300

    Proceso de transicion header listo

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+
+var flag = true
+
 window.addEventListener("scroll",()=>{
     let y = window.scrollY
-    
-    if(y<270){
-        
-    }else{
-
+    console.log(y);
+    if(flag && y>=270){
+        myFunc(y)
+        flag=false
+    }
+    if(!flag && y<270){
+        flag=true
+        myFunc(y)
     }
 })
 
+const myFunc=(scrollY)=>{
+    let headerScroll
+    if(scrollY<270 ){
+        if(document.querySelector(".header-scroll")){
+            headerScroll=document.querySelector(".header-scroll")
+            headerScroll.setAttribute("hidden",true)
+        }
+        if(document.querySelector(".header-scrollend")){
+            headerScroll=document.querySelector(".header-scrollend")
+            headerScroll.className="header-scroll"
+            headerScroll.setAttribute("hidden",true)
+        }
+    }else{
+        headerScroll=document.querySelector(".header-scroll")
+        headerScroll.removeAttribute("hidden")
+        headerScroll.className="header-scrollend"
+        console.log("asdads");
+    }
+}
